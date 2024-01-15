c7f405e679a9f3f0af8eb54a4ca22e6adde82df4
div-styl <gouhmada@gmail.com>
2024-01-15 15:11:45 +0100

    the use of await

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -12,5 +12,5 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """return max_delay"""
     rnd = random.uniform(0, max_delay)
-    asyncio.sleep(rnd)
+    await asyncio.sleep(rnd)
     return rnd
