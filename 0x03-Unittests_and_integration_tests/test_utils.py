4fda3c128aa4ad41caea9fe31afa9f382600c383
gouhmada <gouhmada@gmail.com>
2024-02-02 19:54:43 +0000

    test 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -36,3 +36,25 @@ class TestAccessNestedMap(unittest.TestCase):
         """test that keyerror is raised for the input"""
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """class for testing the json resp"""
+    @parameterized(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False})
+        ]
+    )
+    def test_get_json(
+        self,
+        url: str,
+        test_payload: Dict
+    ):
+        """Test the get json function by mocking the request"""
+        mock_resp = Mock()
+        mock_resp.json.return_value = test_payload
+        with patch('request.get', return_value=mock_resp) as mocked_get:
+            output = get_json(url)
+        mocked_get.assert_called_once_with(url)
+        self.assertEqual(output, test_payload)
