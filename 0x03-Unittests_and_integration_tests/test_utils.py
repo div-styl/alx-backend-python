7131d77820cd2bd37394b45a536d28af4a9eeb13
gouhmada <gouhmada@gmail.com>
2024-02-02 19:31:15 +0000

    notation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Module for testing the utils """
+from typing import Mapping, Sequence, Dict, Any
 from parameterized import parameterized
 import unittest
 from unittest.mock import patch
@@ -17,7 +18,9 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": {"b": 2}}, ("a", "b"), 2),
         ]
     )
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(
+        self, nested_map: Mapping, path: Sequence, expected: Any
+    ):
         """The test method for returns what it is supposed to"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
@@ -27,7 +30,9 @@ class TestAccessNestedMap(unittest.TestCase):
             ({{"a": 1}}, ("a", "b"), "b"),
         ]
     )
-    def test_access_nested_map_exception(self, nested_map, path, expected):
+    def test_access_nested_map_exception(
+        self, nested_map: Mapping, path: Sequence, expected: Any
+    ):
         """test that keyerror is raised for the input"""
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
